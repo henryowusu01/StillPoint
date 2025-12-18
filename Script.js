7d405c98a2c6cfe3a113c4e84cdd598f3a800452
Henry Owusu <St10446847@rcconnect.edu.za>
2025-12-18 13:01:14 +0200

    Initial layout and background setup This is first stage of stillpoint website

---
+++ Script.js
@@ -0,0 +1,9 @@
+ window.addEventListener("load", () => {
+  const content = document.querySelector(".animate");
+
+  setTimeout(() => {
+    content.style.transition = "opacity 1.2s ease, transform 1.2s ease";
+    content.style.opacity = "1";
+    content.style.transform = "translateY(0)";
+  }, 300);
+});
